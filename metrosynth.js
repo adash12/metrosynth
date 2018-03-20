f40bbd470635fe2148b7899b374bf42ee07162f0
cdefran <chrisdefrancisci@gmail.com>
2018-03-20 19:39:46 -0400

    cleaning up crap

--- metrosynth.js
+++ metrosynth.js
@@ -1,4 +1,4 @@
-// joint.js things
+// joint.js setup
 var canvas = $('#canvas');
 var cells = [];
 var graph = new joint.dia.Graph();
@@ -9,38 +9,16 @@ var paper = new joint.dia.Paper({
 	gridSize: 1, 
 	model: graph,
     defaultRouter: { name: 'metro' },
-    // defaultLink: new joint.dia.Link({
-    //     attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' } }
-    // }),
 	validateConnection: function(cellViewS, magnetS, cellViewT, magnetT, end, linkView) {
-		// // Prevent linking from input ports.
-		// if (magnetS && magnetS.getAttribute('port-group') === 'in') return false;
-		// // Prevent linking from output ports to input ports within one element.
-		// if (cellViewS === cellViewT) return false;
-		// // Prevent linking to input ports.
-		// return magnetT && magnetT.getAttribute('port-group') === 'in';
 
         // Prevent links that don't connect to anything 
         // (?) doesn't seem to work
         return cellViewS && cellViewT;
 
 	},
-	// validateMagnet: function(cellView, magnet) {
-	// 	// Note that this is the default behaviour. Just showing it here for reference.
-	// 	// Disable linking interaction for magnets marked as passive (see below `.inPorts circle`).
-	// 	return magnet.getAttribute('magnet') !== 'passive';
-	// }
-
-    // Enable link snapping within 20px lookup radius
-    // snapLinks: { radius: 20 }
 });
 
-// tone.js things
-
-// var synth = new Tone.Synth();
-// var effect = new Tone.FeedbackDelay("4n", 1);
-// effect.wet = 0.5;
-
+// create cells
 cells[0] = new joint.shapes.devs.Model({
   type: 'devs.Model',
   position: {x: 20, y: 20},
@@ -70,7 +48,7 @@ cells[0] = new joint.shapes.devs.Model({
         rect: { fill: '#2ECC71' }
     }
 });
-// setup cells
+// place, annotate cells
 cells[0].translate(140, 100);
 cells[1] = cells[0].clone();
 cells[1].translate(300, 60);
@@ -80,49 +58,15 @@ cells[2] = cells[1].clone();
 cells[2].translate(300, 60);
 cells[2].attr('.label/text', 'Output');
 graph.addCells(cells);
+// tone.js setup
+// create dictionary of toneJS objects
 var idDict = {};
-// create dictionary
-// this way uses labels of elements
-for (var i = 0; i < cells.length; i++) {
-    // var m = ['Cell ' + i + 'has id ' + cells[i].id ].join('');
-    // out(m);
-    idDict[cells[i].id] = cells[i].attr('.label/text');
-};
-// but we could just have the dictionary be toneJS objects
 idDict[cells[0].id] = new Tone.Synth();
 idDict[cells[1].id] = new Tone.FeedbackDelay("4n", 0.5);
 idDict[cells[1].id].wet = 0.5;
 idDict[cells[2].id] = Tone.Master; 
 // array for each "line"/osc of element IDs
 var oscArr = [cells[0].id];
-
-
-// var svgZoom = svgPanZoom('#canvas svg', {
-//   center: false,
-//   zoomEnabled: true,
-//   panEnabled: true,
-//   controlIconsEnabled: true,
-//   fit: false,
-//   minZoom: 0.5,
-//   maxZoom:2,
-//   zoomScaleSensitivity: 0.5
-// });
-
-// (function(){
-//   paper.on('cell:pointerdown', function(){
-//     svgZoom.disablePan();
-//   });
-//   paper.on('cell:pointerup', function(){
-//     svgZoom.enablePan();
-//   });
-  
-//   paper.on('cell:pointerclick', function(e){
-//     message.addClass('visible');
-//     message.html(e.el.textContent+' clicked');
-//   setTimeout(function(){  message.removeClass('visible');
-//                        }, 1000);
-//   });
-// })();
 
 
 // --- event handlers ---------------------------------------------------------
@@ -187,22 +131,6 @@ function removeAudioNode(oscArr, idDict){
 
 // used to add tone.js audio nodes when link is connected
 function connectAudioNode(oscArr, idDict) {
-    // if(idDict[oscArr[0]] === 'Osc'){
-    //     synth = new Tone.Synth();
-    // }
-    // else{ // this would be an error with oscArr
-    //     return;
-    // }
-    // for (var i = 0; i < oscArr.length; i++) {
-    //     if(idDict[oscArr[i]] === 'Output'){
-    //         out('connect to master');
-    //         synth.connect(Tone.Master);
-    //     }
-    //     if(idDict[oscArr[i]] === 'Effect'){
-    //         out('connect to effect');
-    //         synth = synth.connect(effect);
-    //     }
-    // };
     // go through each element, get the corresponding tone.js element
     // then connect current tone.js element to the next one
     // todo: clear elements when links are removed
